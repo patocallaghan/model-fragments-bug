b29b6f7cc188b69ad2169f3b48b141ee445a8c47
Pat O'Callaghan <pat@intercom.io>
2020-05-22 22:29:06 +0100

    Fix test

--- tests/factories/parent.js
+++ tests/factories/parent.js
@@ -2,6 +2,6 @@ import FactoryGuy from 'ember-data-factory-guy';
 
 FactoryGuy.define('parent', {
   default: {
-    children: FactoryGuy.hasMany('child'),
+    children: FactoryGuy.hasMany('child', 1),
   },
 });
