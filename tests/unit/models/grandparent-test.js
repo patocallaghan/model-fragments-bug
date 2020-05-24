d13a0895db265b978ee6a3e0c7e17cc1f0570fde
Pat O'Callaghan <pat@intercom.io>
2020-05-24 14:24:16 +0100

    Update tests to have non-factory guy example

--- tests/unit/models/grandparent-test.js
+++ tests/unit/models/grandparent-test.js
@@ -6,9 +6,24 @@ module('Unit | Model | grandparent', function(hooks) {
   setupTest(hooks);
   setupFactoryGuy(hooks);
 
-  test('grandparent works', function(assert) {
+  test('grandparent works with factory guy', function(assert) {
     let grandparent = make('grandparent', {
       parent: make('parent'),
+    });
+    assert.equal(grandparent.parent.children.firstObject.name, 'Pat', 'Name is Pat');
+  });
+
+  test('grandparent works without factory guy', function(assert) {
+    let store = this.owner.lookup('service:store');
+    let parentClone = store.createFragment('parent', {
+      children: [{
+        name: 'Pat'
+      }]
+    });
+    let grandparent = store.createRecord('grandparent', {
+      parent: {
+        children: parentClone.children,
+      }
     });
     assert.equal(grandparent.parent.children.firstObject.name, 'Pat', 'Name is Pat');
   });
